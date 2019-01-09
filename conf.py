5086528a49f462ddea7b4fd33f0118ed46d285e5
Rodrigo Jorge Rego Moules Rocha <rodrigo.j.rocha@eda.pt>
2019-01-09 17:13:44 -0100

    favicon

--- conf.py
+++ conf.py
@@ -116,6 +116,10 @@ html_logo = "_static/logo_horiz_small.png"
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'SegmaDocumentacaodoc'
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "_static/logoSEGMA_favico.png"
 
 # -- Options for LaTeX output ------------------------------------------------
 
