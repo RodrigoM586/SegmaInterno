66918be7c5e67ff3192b90a64bd0761bafd6a8e0
Rodrigo Jorge Rego Moules da Rocha <rodrigo.j.rocha@eda.pt>
2019-03-27 15:23:01 -0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -103,22 +103,8 @@ html_theme_path = ["_themes", ]
 # further.  For a list of options available for each theme, see the
 # documentation.
 
-#html_theme_options = {
-#    'logo_only': True
-#}
-
- html_theme_options = {
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+html_theme_options = {
+    'logo_only': True
 }
 
 
