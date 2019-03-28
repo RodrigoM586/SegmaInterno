9d9902de619402888600dc73b106d8f21976e856
Rodrigo Jorge Rego Moules da Rocha <rodrigo.j.rocha@eda.pt>
2019-03-28 11:28:35 -0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -139,7 +139,7 @@ htmlhelp_basename = 'SegmaDocumentacaodoc'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/logoSEGMA_favico..ico"
+html_favicon = "_static/logoSEGMA_favico.ico"
 
 # -- Options for LaTeX output ------------------------------------------------
 
